abda03d138fb9e0368088b390d7301846ca13c55
Ted <damquangkhoa02@gmail.com>
2020-01-07 23:30:22 +0700

    Add docker and django rest

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'blog',
     'taggit',
     'django.contrib.sites',
+    
 ]
 
 MIDDLEWARE = [
@@ -77,9 +78,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db', # set in docker-compose.yml
+        'PORT': 5432 # default postgres port
     }
 }
 
